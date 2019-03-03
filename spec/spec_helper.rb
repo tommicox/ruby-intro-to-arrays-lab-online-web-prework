525f5f12ff59ba2d3594e91901dba6970b4f3230
Tommi Cox <tommicox@gmail.com>
2019-03-03 22:20:51 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,5 +16,5 @@ def array_with_two_elements
 end
 
   def first_element("taylor_swift")
-   taylor_swift = ["Welcome to New York","Blank Space", "Style", "Out of the Woods"]
+   taylor_swift = ["Welcome to New York","Blank Space", "Style", "Out of The Woods"]
   end
