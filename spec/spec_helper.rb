521ffe35e20bee5eeadfd757ea751f79d9f81a16
Tommi Cox <tommicox@gmail.com>
2019-03-03 22:32:22 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,5 +22,7 @@ end
   def third_element(taylor_swift)
     taylor_swift[2]
   end
-  
+  def first_element_with_array_methods(south_east_asia)
+    south_east_asia = ["Thailand", "Cambodia", "Singapore","Myanmar"]
+    
   
